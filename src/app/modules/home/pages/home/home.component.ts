6c8a3194be08b72fb0e8cc5649f6ffcd844863ed
AlirezaRezaei <cavalier.boy76@gmail.com>
2021-03-31 13:10:35 +0430

    add product list animation

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  query,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, OnInit, Optional } from '@angular/core';
 import { Product } from 'src/app/shared/models/product.models';
 import { ProductSharedService } from '../../../../shared/services/product-shared.service';
 
@@ -6,6 +15,24 @@ import { ProductSharedService } from '../../../../shared/services/product-shared
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      transition('* => *', [
+        query(
+          ':enter',
+          style({ opacity: 0, transform: 'translateX(-100px)' }),
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          stagger('1000ms', [
+            animate('1s', style({ opacity: 1, transform: 'translateX(0px)' })),
+          ]),
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   public productList: Product[] = [];
@@ -14,7 +41,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getProduct();
   }
-  
+
   getProduct() {
     this.productSharedService.getProductList().subscribe((data) => {
       this.productList = data;
